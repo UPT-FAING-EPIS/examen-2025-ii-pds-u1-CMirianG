b0d93addeeeb0daf8d03f882cbefc3c7a0e01705
CMirianG <mc2021071083@virtual.upt.pe>
2025-09-21 09:55:25 -0500

    Cambios

--- backend/AttendanceSystem.API/Program.cs
+++ backend/AttendanceSystem.API/Program.cs
@@ -31,7 +31,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+            policy.WithOrigins("http://localhost:5173", "http://localhost:4000")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
